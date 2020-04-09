9a10c9dc9f0ab6a80865101584c6143b99d205d8
YukiNarayama <yuki-narayama@outlook.jp>
2020-04-09 17:47:16 +0900

    口から顔に認識に変更
    口の認識精度あんまし良くないわね

---
+++ Python/main.py
@@ -0,0 +1,56 @@
+import cv2
+
+if __name__ == '__main__':
+    # 定数定義
+    ESC_KEY = 27     # Escキー
+    INTERVAL= 33     # 待ち時間
+    FRAME_RATE = 30  # fps
+
+    ORG_WINDOW_NAME = "org"
+    GAUSSIAN_WINDOW_NAME = "gaussian"
+
+    DEVICE_ID = 0
+
+    # 分類器の指定
+    #cascade_file = "Python\haarcascade_mcs_mouth.xml"
+    cascade_file = "Python\haarcascade_frontalface_alt_tree.xml"
+
+    cascade = cv2.CascadeClassifier(cascade_file)
+
+    # カメラ映像取得
+    cap = cv2.VideoCapture(DEVICE_ID)
+
+    # 初期フレームの読込
+    end_flag, c_frame = cap.read()
+    height, width, channels = c_frame.shape
+
+    # ウィンドウの準備
+    cv2.namedWindow(GAUSSIAN_WINDOW_NAME)
+
+    # 変換処理ループ
+    while end_flag == True:
+
+        # 画像の取得と顔の検出
+        img = c_frame
+        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        face_list = cascade.detectMultiScale(img,scaleFactor=1.1, minNeighbors=2, minSize=(50, 50))
+        # 検出した顔に印を付ける
+        for (x, y, w, h) in face_list:
+            color = (0, 0, 225)
+            pen_w = 3
+            cv2.rectangle(img, (x, y), (x+w, y+h), color, thickness = pen_w)
+
+        # フレーム表示
+        cv2.imshow(GAUSSIAN_WINDOW_NAME, img)
+
+        # Escキーで終了
+        key = cv2.waitKey(INTERVAL)
+        if key == ESC_KEY:
+            break
+
+        # 次のフレーム読み込み
+        end_flag, c_frame = cap.read()
+
+    # 終了処理
+    cv2.destroyAllWindows()
+    cap.release()
